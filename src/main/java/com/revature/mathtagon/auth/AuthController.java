b591805278b1a583ed042afdbc240a89e8d39dd7
MrNoms <tiddle96@gmail.com>
2022-06-18 19:56:13 -0700

    Auth servlet block by User servlet. Can be tested in this state

--- src/main/java/com/revature/mathtagon/auth/AuthController.java
+++ src/main/java/com/revature/mathtagon/auth/AuthController.java
@@ -1,12 +1,37 @@
 package com.revature.mathtagon.auth;
 
+import com.revature.mathtagon.auth.dtos.requests.LoginRequest;
+import com.revature.mathtagon.auth.dtos.responses.Principal;
 import com.revature.mathtagon.util.annotations.Inject;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
     @Inject
     private final TokenService mTokenService;
+
+    @Inject
+    @Autowired
+    public AuthController(TokenService tServ) {
+        mTokenService = tServ;
+    }
+
+    @PostMapping(consumes = "application/json", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody Principal login(@RequestBody LoginRequest req, HttpServletResponse resp) {
+        Principal p;
+
+        if(req.getUsername().equals("admin") && req.getPassword().equals("revature"))
+            p = new Principal(UUID.randomUUID().toString(), req.getUsername());
+        else throw new RuntimeException("Access denied");
+
+        String token = mTokenService.generateToken(p);
+        resp.setHeader("Authorization", token);
+        return p;
+    }
 }
