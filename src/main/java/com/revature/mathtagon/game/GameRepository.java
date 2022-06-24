f8d796964582800467df9b61146451bd74600261
Jeremiah Bolden <Jeremiah791@revature.net>
2022-06-24 11:05:12 -0400

    point adding command

--- src/main/java/com/revature/mathtagon/game/GameRepository.java
+++ src/main/java/com/revature/mathtagon/game/GameRepository.java
@@ -1,5 +1,6 @@
 package com.revature.mathtagon.game;
 
+import com.revature.mathtagon.game.dtos.requests.NewScoreRequest;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
@@ -7,6 +8,6 @@ import org.springframework.data.repository.CrudRepository;
 public interface GameRepository extends CrudRepository<Game, String> {
 
     @Modifying
-    @Query(value = "INSERT INTO game SET score = ? WHERE userID = ?")
-    void addPoints(Integer score, String userID);
+    @Query(value = "INSERT INTO games (score,gametype,userID) VALUES (?1,?2,?3)",nativeQuery = true)
+    void record(Integer score, Integer gameType, String userID);
 }
